221e4e5fd2fdfdefead8ef38a5d0747c7510b16a
puliv <pverapavez@gmail.com>
2018-12-04 23:08:38 -0300

    avance con index.js y falta correr los test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-/* Acá va tu código */
+
+    document.getElementById("botonUno").addEventListener("click", (evento)=>{
+        evento.preventDefault();
+
+         document.getElementById("partTwo").style.display="block";
+    });
+    document.getElementById("botonCifrar").addEventListener("click", (evento)=>{
+        evento.preventDefault();
+
+         let inputText= document.getElementById("textoIgresado").value; 
+         let offset= document.getElementById("offSet").value; 
+         let resultFinal= cipher.encode(inputText, offset);
+         document.getElementById("textoReady").innerHTML= resultFinal;
+
+         document.getElementById("partThree").style.display="block";
+    });
+    document.getElementById("botonDecifrar").addEventListener("click", (evento)=>{
+        evento.preventDefault();
+
+         let inputText= document.getElementById("textoIgresado").value;
+         let offset= document.getElementById("offSet").value;
+         let resultFinal= cipher.decode(inputText, offset);
+         document.getElementById("textoReady").innerHTML= resultFinal;
+
+         document.getElementById("partThree").style.display="block";
+    });
